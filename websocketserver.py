cf7d51a729170dfcddb164ecf452abbacd49f5d6
whiskersthecat <60399984+whiskersthecat@users.noreply.github.com>
2023-10-21 20:10:45 -0700

    Add files via upload

--- websocketserver.py
+++ websocketserver.py
@@ -1,6 +1,16 @@
 
 import asyncio
- 
+# TO SETUP BUGGLY
+#   >> 1. Find your devices LOCAL IP address in the Wifi settings
+# 			this is the IP assigned by the router. This can change occasionally when the lease expires!
+#   >> 2. Change the local address below to bind this socket and the HTTP server socket
+#    >>   Also, change the local address in the HTTP server (server.py) variable HOSTNAME
+#   >> 3. Then, sign into the router to enable port forwarding 8000 - 9000 to this device
+#   >> 4. Start to terminal sessions and run both python scripts
+#   >> 5. Determine your public IP address by using a google search tool, and share this 'ADDRESS:9000' to others
+LOCAL_ADDRESS = '10.0.0.141'
+
+
 import websockets
 import json
 
@@ -28,15 +38,7 @@ appleHarvestSpeed = 12
 
 numcards = 5
 
-# this is the IP assigned by the router. This can change occasionally when the lease expires!
-#   >> 1. In this case, find the new IP address in the Wifi settings
-#   >> 2. Change the local address below to bind this socket and the HTTP server socket
-#    >>   Also, change the local address in the HTTP server (server.py) variable HOSTNAME
-#   >> 3. Then, sign into the router to enable port forwarding 8000 - 9000 to this device
-#   >> 4. Start to terminal sessions and run both python scripts
-#   >> 5. Determine your public IP address by using a google search tool, and share this 'ADDRESS:9000' to others
-LOCAL_ADDRESS = '192.168.1.14'
-LOCAL_ADDRESS = '10.0.0.141'
+
 
 
 # number 9 (nap) is discontinued as level0
